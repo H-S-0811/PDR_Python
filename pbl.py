ac383c02bb36a50786110ad7864e8d37d7dd84ef
H-S-0811 <sougo08110811@gmail.com>
2024-12-17 11:08:07 +0900

    機能実装,pdフレーム前

--- pbl.py
+++ pbl.py
@@ -1,21 +1,31 @@
 import csv
 import numpy as np
+import math
+from numpy import linalg as LA
 
 csv_data = []
 delta_t: float = 1/120
-
+j = 0
+posi = []
 
 def Cul_Omega(data: list) -> np.ndarray:
-    x_data = [0,-1 * float(data[6]),float(data[5])]
-    y_data = [float(data[6]),0,-1* float(data[4])]
-    z_data = [-1 *float(data[5]),float(data[4]),0]
+    x_data = [0,-1*float(data[6]),float(data[5])]
+    y_data = [float(data[6]),0,-1*float(data[4])]
+    z_data = [-1*float(data[5]),float(data[4]),0]
+    M_list = [x_data,y_data,z_data]
+    return np.array(M_list)
+
+def Cul_Omega_k(data: np.ndarray) -> np.ndarray:
+    x_data = [0,1 * float(data[2]), -float(data[1])]
+    y_data = [-float(data[2]),0,1* float(data[0])]
+    z_data = [1 *float(data[1]),-float(data[0]),0]
     M_list = [x_data,y_data,z_data]
     return np.array(M_list)
 
 def Cul_S(data: np.ndarray) -> np.ndarray:
-    x_data = [0,-1 * float(data[2]),float(data[1])]
-    y_data = [float(data[2]),0,-1* float(data[0])]
-    z_data = [-1 *float(data[1]),float(data[0]),0]
+    x_data = [0, -1 * float(data[2, 0]), float(data[1, 0])]
+    y_data = [float(data[2, 0]), 0, -1 * float(data[0, 0])]
+    z_data = [-1 * float(data[1, 0]), float(data[0, 0]), 0]
     M_list = [x_data,y_data,z_data]
     return np.array(M_list)
 
@@ -40,10 +50,9 @@ def Cul_acc_n(Ck: np.ndarray,Ck_1: np.ndarray,data: list) -> np.ndarray:
 def Cul_F(Sk: np.ndarray) -> np.ndarray:
     a = np.vstack([np.vstack([np.identity(3,dtype=float),np.zeros((3,3))]),-1 * Sk*delta_t])
     b = np.vstack([np.vstack([np.zeros((3,3)),np.identity(3,dtype=float)]),np.zeros((3,3))])
-    c = np.vstack([np.vstack([np.zeros((3,3)),delta_t * np.identity(3,dtype=float)]),np.identity(3,dtype=float)])
+    c = np.vstack([np.vstack([np.zeros((3,3)),np.identity(3,dtype=float) * delta_t]),np.identity(3,dtype=float)])
     return np.concatenate([a,b,c], axis=1)
 
-    
 
 with open("data/1.csv") as f:
     reader = csv.reader(f,delimiter =';')
@@ -53,25 +62,64 @@ with open("data/1.csv") as f:
         i = i+1
 
 Omegak_1 = Cul_Omega(csv_data[1])
-Ck_1 = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak_1 * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak_1*delta_t))
-Acc_N_1 = Cul_acc_n(Ck_1,Ck_1,csv_data[1])
+roll = math.atan(float(csv_data[1][2])/float(csv_data[1][3]))
+pitch = -math.asin(float(csv_data[1][1])/9.8)
+C_x = [math.cos(pitch),math.sin(roll)*math.sin(pitch),math.cos(roll)*math.sin(pitch)]
+C_y = [0,math.cos(roll),-math.sin(roll)]
+C_z = [-math.sin(pitch),math.sin(roll)*math.cos(pitch),math.cos(roll)*math.cos(pitch)]
+C_list = [C_x,C_y,C_z]
+C = np.array(C_list)
+
+# Ck_1 = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak_1 * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak_1*delta_t))
+Ck_1 = C
+# Acc_N_1 = Cul_acc_n(C,Ck_1,csv_data[1])
+a_S = np.array([float(csv_data[1][1]), float(csv_data[1][2]), float(csv_data[1][3])])
+Acc_N_1 = C @ a_S
 vk_1 = np.array([[0],[0],[0]])
 pk_1 = np.array([[0],[0],[0]])
-P = np.zeros((9,9))
+Pk_1 = np.zeros((9,9))
 gra = np.array([[0],[0],[9.8]])
+Q = [0.01,0.01,0.01,0,0,0,0.01,0.01,0.01]
+Qk = np.linalg.matrix_power(np.diag(Q)*delta_t,2)
+r = [0.01**2,0.01**2,0.01**2]
+R = np.diag(r)
+H = np.hstack([np.hstack([np.zeros((3,3)),np.zeros((3,3))]),np.identity(3,dtype=float)])
 
-for i in range(2,120):
+for i in range(2,len(csv_data)):
+# for i in range(2,5):
+    delta_t = float(csv_data[i][0]) - float(csv_data[i-1][0])
+    csv_data[i][4] = str(float(csv_data[i][4]) - (-0.0156))
+    csv_data[i][5] = str(float(csv_data[i][5]) - (-0.0101))
+    csv_data[i][6] = str(float(csv_data[i][6]) - (-0.0020))
     Omegak = Cul_Omega(csv_data[i])
-    Ck = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak * delta_t , 2 * np.identity(3,dtype=float) - Omegak*delta_t)
+    g_k = np.array([csv_data[i][4],csv_data[i][5],csv_data[i][6]])
+    Ck = Ck_1 @ (2 * np.identity(3,dtype=float) + (Omegak * delta_t)) @ np.linalg.inv(2 * np.identity(3,dtype=float) - (Omegak*delta_t))
+
+
     Acc_N = Cul_acc_n(Ck,Ck_1,csv_data[i])
     vk = vk_1 + (Acc_N + Acc_N_1 - 2*gra)* delta_t / 2
     pk = pk_1 + (vk+vk_1)* delta_t / 2
-    print(f"{pk[0][0]},{pk[1][0]},{pk[2][0]}")
-    Ck_1 = Ck
-    Acc_N_1 = Acc_N
-    vk_1 = vk
-    pk_1 = pk
+    # print(f"{pk[0][0]},{pk[1][0]},{pk[2][0]}")
+    
+    Acc_N_1 = np.copy(Acc_N)
+    vk_1 = np.copy(vk)
+    pk_1 = np.copy(pk)
     Sk = Cul_S(Acc_N)
     Fk = Cul_F(Sk)
-   
+    Pk = np.matmul(Fk,Pk_1,Fk.T) + Qk
+    # if LA.norm(g_k) < 0.6:
+    #     k = np.linalg.inv(H @ Pk @ H.T + R)
+    #     Kk = Pk @ H.T @ k
+    #     a = Kk @ vk
+    #     Pk = (np.identity(9,dtype=float) - Kk @ H) @ Pk
+    #     Omegaep_k = Cul_Omega_k(a)
+    #     Ck = Cul_matrix(2 * np.identity(3,dtype=float) + Omegaep_k * delta_t , 2 * np.identity(3,dtype=float) - Omegaep_k*delta_t)
 
+    #     pk = pk - a[3:6]
+    #     vk = vk - a[6:]
+    #     vk_1 = vk
+    #     pk_1 = pk
+    
+    Pk_1 = np.copy(Pk)
+    Ck_1 = np.copy(Ck)
+    posi.append(pk)
--- test_pbl.py
+++ test_pbl.py
@@ -1,6 +1,10 @@
 import unittest
 import numpy as np
-from pbl import Cul_Omega,Cul_S,Cul_matrix
+from pbl import Cul_Omega,Cul_S,Cul_matrix,Cul_acc_n
+import csv
+
+csv_data = []
+delta_t: float = 1/120
 
 class Testpbl(unittest.TestCase):
     data = [0,-4.773060,-1.769106,8.427839,-0.030030,0.022133,0.002239]
@@ -19,13 +23,31 @@ class Testpbl(unittest.TestCase):
         ans = [[0,-0.002239,0.022133],[0.002239,0,0.030030],[-0.022133,-0.030030,0]]
         np.testing.assert_array_equal(result,ans)
 
-    def test_Cul_matrix():
+    def test_Cul_matrix(self):
         data = [0,-4.773060,-1.769106,8.427839,-0.030030,0.022133,0.002239]
         Omegak = Cul_Omega(data)
-        result = Cul_matrix(Omegak,Omegak)
-        ans = [[-0.00049488281,-0.00066465399,-0.00006723717],[-0.00066465399,-0.000906814021,0.000049555787],[-0.00006723717,0.000049555787,-0.001]]
+        delta_t: float = 1/120
+        result = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak*delta_t))
+        ans = [[1,-1.86813*(10**-5),1.84442*(10**-4)],[1.86572*(10**-5),1,2.5024*(10**-4)],[-1.84444*(10**-4),-2.5025*(10**-4),1]]
         np.testing.assert_array_almost_equal(result,ans)
+
+    def test_Cul_acc_n(self):
+        Omegak_1 = Cul_Omega(csv_data[1])
+        Ck_1 = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak_1 * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak_1*delta_t))
+        Omegak = Cul_Omega(csv_data[2])
+        Ck = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak*delta_t))
+        result = Cul_acc_n(Ck,Ck_1,csv_data[1])   
+        ans = [[-4.763233],[-1.678572],[8.581781]]
+        np.testing.assert_array_almost_equal(result,ans,5)
+        
+
 
 
 if __name__ == '__main__':
+    with open("data/1.csv") as f:
+        reader = csv.reader(f,delimiter =';')
+        i = 0
+        for row in reader:
+            csv_data.append(row)
+            i = i+1
     unittest.main()
