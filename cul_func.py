c92c9f0eab3f9c132d6be6576294188a1cc14fa1
H-S-0811 <sougo08110811@gmail.com>
2025-01-23 18:44:46 +0900

    フォーマット後

--- cul_func.py
+++ cul_func.py
@@ -4,63 +4,86 @@ from numpy import linalg as LA
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def Omega(data: pd.Series|pd.DataFrame) -> np.ndarray:
-    x_data = [0,-1*data["gyr_z"],data["gyr_y"]]
-    y_data = [data["gyr_z"],0,-1*data["gyr_x"]]
-    z_data = [-1*data["gyr_y"],data["gyr_x"],0]
-    M_list = [x_data,y_data,z_data]
+
+def Omega(data: pd.Series | pd.DataFrame) -> np.ndarray:
+    data = data.copy()
+    x_data = [0, -1 * data["gyr_z"], data["gyr_y"]]
+    y_data = [data["gyr_z"], 0, -1 * data["gyr_x"]]
+    z_data = [-1 * data["gyr_y"], data["gyr_x"], 0]
+    M_list = [x_data, y_data, z_data]
     return np.array(M_list)
 
+
 def Omega_k(data: np.ndarray) -> np.ndarray:
-    x_data = [0,1 * float(data[2]), -float(data[1])]
-    y_data = [-float(data[2]),0,1* float(data[0])]
-    z_data = [1 *float(data[1]),-float(data[0]),0]
-    M_list = [x_data,y_data,z_data]
+    x_data = [0, 1 * float(data[2]), -float(data[1])]
+    y_data = [-float(data[2]), 0, 1 * float(data[0])]
+    z_data = [1 * float(data[1]), -float(data[0]), 0]
+    M_list = [x_data, y_data, z_data]
     return np.array(M_list)
+
 
 def S(data: np.ndarray) -> np.ndarray:
     x_data = [0, -1 * float(data[2, 0]), float(data[1, 0])]
     y_data = [float(data[2, 0]), 0, -1 * float(data[0, 0])]
     z_data = [-1 * float(data[1, 0]), float(data[0, 0]), 0]
-    M_list = [x_data,y_data,z_data]
+    M_list = [x_data, y_data, z_data]
     return np.array(M_list)
 
+
 def Matrix(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-    if a.shape != (3,3):
+    if a.shape != (3, 3):
         raise Exception(a.shape)
-    if not (b.shape == (3,3) or b.shape == (3,1)):
+    if not (b.shape == (3, 3) or b.shape == (3, 1)):
         raise Exception(b.shape)
     return a @ b
 
-def Acc_n(Ck: np.ndarray,Ck_1: np.ndarray,data:pd.Series|pd.DataFrame) -> np.ndarray: 
+
+def Acc_n(
+    Ck: np.ndarray, Ck_1: np.ndarray, data: pd.Series | pd.DataFrame
+) -> np.ndarray:
+    data = data.copy()
     a_S = np.array(
         [float(data.at["acc_x"]), float(data.at["acc_y"]), float(data.at["acc_z"])]
     )
-    a_S = a_S.reshape(3,1)
-    
-    if a_S.shape != (3,1):
+    a_S = a_S.reshape(3, 1)
+
+    if a_S.shape != (3, 1):
         raise Exception(a_S.shape)
 
-    return Matrix((Ck+Ck_1),a_S)*1/2
+    return Matrix((Ck + Ck_1), a_S) * 1 / 2
+
 
 def F(Sk: np.ndarray, delta_t: float) -> np.ndarray:
-    if Sk.shape != (3,3):
+    if Sk.shape != (3, 3):
         raise Exception(Sk.shape)
-    a = np.vstack([np.vstack([np.identity(3,dtype=float),np.zeros((3,3))]),-1 * delta_t * Sk])
-    b = np.vstack([np.vstack([np.zeros((3,3)),np.identity(3,dtype=float)]),np.zeros((3,3))])
-    c = np.vstack([np.vstack([np.zeros((3,3)),np.identity(3,dtype=float) * delta_t]),np.identity(3,dtype=float)])
-    return np.concatenate([a,b,c], axis=1)
+    a = np.vstack(
+        [np.vstack([np.identity(3, dtype=float), np.zeros((3, 3))]), -1 * delta_t * Sk]
+    )
+    b = np.vstack(
+        [np.vstack([np.zeros((3, 3)), np.identity(3, dtype=float)]), np.zeros((3, 3))]
+    )
+    c = np.vstack(
+        [
+            np.vstack([np.zeros((3, 3)), np.identity(3, dtype=float) * delta_t]),
+            np.identity(3, dtype=float),
+        ]
+    )
+    return np.concatenate([a, b, c], axis=1)
 
-def Pk(Fk: np.ndarray,Pk_1: np.ndarray,Qk: np.ndarray) -> np.ndarray:
+
+def Pk(Fk: np.ndarray, Pk_1: np.ndarray, Qk: np.ndarray) -> np.ndarray:
     Fk = Fk.copy()
-    return (Fk @ Pk_1 @ Fk.T) + Qk 
+    return (Fk @ Pk_1 @ Fk.T) + Qk
+
 
 def Kk(H: np.ndarray, Pk: np.ndarray, R: np.ndarray) -> np.ndarray:
     k = np.linalg.inv((H @ Pk @ H.T) + R)
     return Pk @ H.T @ k
 
-def update_Pk(Kk: np.ndarray, H: np.ndarray, Pk: np.ndarray) -> np.ndarray:
-    return (np.identity(9,dtype=float) - (Kk @ H)) @ Pk
 
-def update_C(Ck_1 :np.ndarray, Ck_2 : np.ndarray, Ck : np.ndarray):
+def update_Pk(Kk: np.ndarray, H: np.ndarray, Pk: np.ndarray) -> np.ndarray:
+    return (np.identity(9, dtype=float) - (Kk @ H)) @ Pk
+
+
+def update_C(Ck_1: np.ndarray, Ck_2: np.ndarray, Ck: np.ndarray):
     return Ck_1 @ np.linalg.inv(Ck_2) @ Ck
--- pbl.py
+++ pbl.py
@@ -16,46 +16,49 @@ j = 0
 posi = []
 vel = []
 ans = []
-posi_E =[]
+posi_E = []
 vel_E = []
 
-with open("data/1.csv") as f:
-    reader = csv.reader(f,delimiter =';')
-    i = 0
-    for row in reader:
-        csv_data.append(row)
-        i = i+1
-
-data = pd.read_csv("data/1.csv",sep=";")
+data = pd.read_csv("data/1.csv", sep=";")
 data_Test = data.copy()
-first = data.loc[0,:]
+first = data.loc[0, :]
 Omegak_1 = Cul.Omega(first)
 
 # numpyの値表示設定
-np.set_printoptions(formatter={'float': '{:.5e}'.format})
+np.set_printoptions(formatter={"float": "{:.5e}".format})
 
-roll = math.atan(float(first.at["acc_y"])/float(first.at["acc_z"]))
-pitch = -math.asin(float(first.at["acc_x"])/9.8)
-C_x = [math.cos(pitch),math.sin(roll)*math.sin(pitch),math.cos(roll)*math.sin(pitch)]
-C_y = [0,math.cos(roll),-math.sin(roll)]
-C_z = [-math.sin(pitch),math.sin(roll)*math.cos(pitch),math.cos(roll)*math.cos(pitch)]
-C_list = [C_x,C_y,C_z]
-C = np.array(C_list,dtype=np.float64)
-time_k =  np.copy(first["time"])
+roll = math.atan(float(first.at["acc_y"]) / float(first.at["acc_z"]))
+pitch = -math.asin(float(first.at["acc_x"]) / 9.8)
+C_x = [
+    math.cos(pitch),
+    math.sin(roll) * math.sin(pitch),
+    math.cos(roll) * math.sin(pitch),
+]
+C_y = [0, math.cos(roll), -math.sin(roll)]
+C_z = [
+    -math.sin(pitch),
+    math.sin(roll) * math.cos(pitch),
+    math.cos(roll) * math.cos(pitch),
+]
+C_list = [C_x, C_y, C_z]
+C = np.array(C_list, dtype=np.float64)
+time_k = np.copy(first["time"])
 # Ck_1 = Cul_matrix(2 * np.identity(3,dtype=float) + Omegak_1 * delta_t , np.linalg.inv(2 * np.identity(3,dtype=float) - Omegak_1*delta_t))
 Ck_1 = C
 # Acc_N_1 = Cul_acc_n(C,Ck_1,csv_data[1])
-a_S = np.array([(first["acc_x"]), first["acc_y"], first["acc_z"]],dtype=np.float64)
-Acc_N_1 = np.inner(C,a_S)
-Acc_N_1 = Acc_N_1.reshape(3,1)
-vk_1 = np.array([[0],[0],[0]],dtype=np.float64)
-pk_1 = np.array([[0],[0],[0]],dtype=np.float64)
-Pk_1 = np.zeros((9,9),dtype=np.float64)
-gra = np.array([[0],[0],[9.8]])
-Q = [0.01,0.01,0.01,0,0,0,0.01,0.01,0.01]
-r = [0.01**2,0.01**2,0.01**2]
+a_S = np.array([(first["acc_x"]), first["acc_y"], first["acc_z"]], dtype=np.float64)
+Acc_N_1 = np.inner(C, a_S)
+Acc_N_1 = Acc_N_1.reshape(3, 1)
+vk_1 = np.array([[0], [0], [0]], dtype=np.float64)
+pk_1 = np.array([[0], [0], [0]], dtype=np.float64)
+Pk_1 = np.zeros((9, 9), dtype=np.float64)
+gra = np.array([[0], [0], [9.8]])
+Q = [0.01, 0.01, 0.01, 0, 0, 0, 0.01, 0.01, 0.01]
+r = [0.01**2, 0.01**2, 0.01**2]
 R = np.diag(r)
-H = np.hstack([np.hstack([np.zeros((3,3)),np.zeros((3,3))]),np.identity(3,dtype=np.float64)])
+H = np.hstack(
+    [np.hstack([np.zeros((3, 3)), np.zeros((3, 3))]), np.identity(3, dtype=np.float64)]
+)
 
 for index, row in data[1:].iterrows():
     shape = (9, 1)
@@ -66,46 +69,45 @@ for index, row in data[1:].iterrows():
     row["gyr_y"] = row["gyr_y"] - (-0.0101)
     row["gyr_z"] = row["gyr_z"] - (-0.0020)
     Omegak = Cul.Omega(row)
-    a_S = np.array([(row["acc_x"]), row["acc_y"], row["acc_z"]],dtype=np.float64)
-    g_k = np.array([row["gyr_x"],row["gyr_y"],row["gyr_z"]],dtype=np.float64)
-    I = np.identity(3,dtype=np.float64)
+    a_S = np.array([(row["acc_x"]), row["acc_y"], row["acc_z"]], dtype=np.float64)
+    g_k = np.array([row["gyr_x"], row["gyr_y"], row["gyr_z"]], dtype=np.float64)
+    I = np.identity(3, dtype=np.float64)
     C_1 = 2 * I + (Omegak * delta_t)
     C_2 = 2 * I - (Omegak * delta_t)
     Ck = (Ck_1 @ C_1) @ np.linalg.inv(C_2)
-    Acc_N = np.inner(0.5 * (Ck+Ck_1),a_S)
-    Acc_N = Acc_N.reshape(3,1)
-    vk = vk_1 + ((Acc_N - gra) + (Acc_N_1 - gra))* delta_t / 2
-    pk = pk_1 + (vk+vk_1)* delta_t / 2
+    Acc_N = np.inner(0.5 * (Ck + Ck_1), a_S)
+    Acc_N = Acc_N.reshape(3, 1)
+    vk = vk_1 + ((Acc_N - gra) + (Acc_N_1 - gra)) * delta_t / 2
+    pk = pk_1 + (vk + vk_1) * delta_t / 2
     # print(f"{pk[0][0]},{pk[1][0]},{pk[2][0]}")
-    
+
     Acc_N_1 = np.copy(Acc_N)
     Sk = Cul.S(Acc_N)
-    Fk = Cul.F(Sk,delta_t)
-    Qk = np.linalg.matrix_power(np.diag(Q)*(delta_t),2)
+    Fk = Cul.F(Sk, delta_t)
+    Qk = np.linalg.matrix_power(np.diag(Q) * (delta_t), 2)
 
     # Pk = np.matmul(Fk,Pk_1,Fk.T) + Qk
-    Pk = Cul.Pk(Fk,Pk_1,Qk)
+    Pk = Cul.Pk(Fk, Pk_1, Qk)
 
-    if LA.norm(g_k,ord=2) < 0.6:
-    # if np.sqrt(row["gyr_x"] ** 2 + row["gyr_y"] ** 2 + row["gyr_z"] ** 2)  < 0.6:
+    if LA.norm(g_k, ord=2) < 0.6:
+        # if np.sqrt(row["gyr_x"] ** 2 + row["gyr_y"] ** 2 + row["gyr_z"] ** 2)  < 0.6:
         # k = np.linalg.inv(H @ Pk @ H.T + R)
         # Kk = Pk @ H.T @ k
-        Kk = Cul.Kk(H,Pk,R)
+        Kk = Cul.Kk(H, Pk, R)
 
         a = Kk @ vk
         A = a
         # Pk = (np.identity(9,dtype=float) - Kk @ H) @ Pk
-        Pk = Cul.update_Pk(Kk,H,Pk)
+        Pk = Cul.update_Pk(Kk, H, Pk)
         # ここまで正解
 
         Omegaep_k = Cul.Omega_k(a[0:3])
 
-        Ck_1_L = 2 * np.identity(3,dtype=np.float64) + Omegaep_k * delta_t
-        Ck_2_L = 2 * np.identity(3,dtype=np.float64) - Omegaep_k * delta_t
-        Ck = Cul.update_C(Ck_1_L,Ck_2_L,Ck)
+        Ck_1_L = 2 * np.identity(3, dtype=np.float64) + Omegaep_k * delta_t
+        Ck_2_L = 2 * np.identity(3, dtype=np.float64) - Omegaep_k * delta_t
+        Ck = Cul.update_C(Ck_1_L, Ck_2_L, Ck)
         pk = pk - a[3:6]
         vk = vk - a[6:]
-
 
     vk_1 = np.copy(vk)
     pk_1 = np.copy(pk)
@@ -141,17 +143,19 @@ for index, row in data[1:].iterrows():
 
 # 2D
 for i in range(len(posi)):
-    z= [posi[i][0][0],posi[i][1][0]]
+    z = [posi[i][0][0], posi[i][1][0]]
     ans.append(z)
 # ans = pd.read_csv("ans.csv")
 # ans.columns = ["X","Y","Z"]
-ANS = pd.DataFrame(ans[1:10000],columns=['X', 'Y'])
-fig = go.Figure(data=[
-    go.Scatter(x=ANS["X"], y=ANS["Y"], name="acc_x"),
-])
+ANS = pd.DataFrame(ans[1:], columns=["X", "Y"])
+fig = go.Figure(
+    data=[
+        go.Scatter(x=ANS["X"], y=ANS["Y"], name="acc_x"),
+    ]
+)
 fig.update_yaxes(
-    scaleanchor = "x",
-    scaleratio = 1,
-  )
-fig.update_scenes(aspectmode='data')
+    scaleanchor="x",
+    scaleratio=1,
+)
+fig.update_scenes(aspectmode="data")
 fig.show()
